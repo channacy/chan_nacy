b7ac3bdf803837ed8cfca45cb6620ff6cc9b00d4
channacy <68759286+channacy@users.noreply.github.com>
2024-09-28 16:57:04 -0400

    updated main page

--- channacy/src/components/Navbar.tsx
+++ channacy/src/components/Navbar.tsx
@@ -1,50 +1,95 @@
 import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <>
-        <div class="sidebar">
+  return (
+    <>
+      <div class="navbar">
         <ul>
-            <li>
-                <a class="header" href="/">Channacy Un</a>
-                <a class="subheader" href="#">About</a>
-            </li>
-            <li>
-                <a class="header" href="#">Projects</a>
-                <a class="subheader" href="#">Web</a>
-                <a class="subheader" href="#">AI/ML</a>
-                <a class="subheader" href="#">Mobile</a>
-                <a class="subheader" href="#">Other</a>
-            </li>
-            <li>
-                <a class="header" href="#">Education</a>
-                <a class="subheader" href="#">Courses</a>
-            </li>
-            <li>
-                <a class="header" href="#">Experience</a>
-                <a class="subheader" href="#">Projects</a>
-            </li>
-            <li>
-                <a class="header" href="#">Blog</a>
-                <a class="subheader" href="#">Sept 2024</a>
-            </li>
-            <li>
-                <a class="header" href="#">Contact</a>
-                <a class="subheader" href="#">GitHub</a>
-                <a class="subheader" href="#">Gmail</a>
-                <a class="subheader" href="#">Linkedin</a>
-            </li>
-        </ul> 
-        <div class="social_media">
-          <a href="https://github.com/channacy" target="_blank"><img src="assets/github.png"/></a>
-          <a href="mailto:channacyun@gmail.com" target="_blank"><img src="assets/gmail.png"/></a>
-          <a href="https://www.linkedin.com/in/channacy-un/" target="_blank"><img src="assets/linkedin.png"/></a>
+          <li>
+            <a class="header" href="/">
+              Channacy Un
+            </a>
+            <a class="subheader" href="#">
+              About
+            </a>
+          </li>
+          <li>
+            <a class="header" href="#">
+              Projects
+            </a>
+            <a class="subheader" href="#">
+              Web
+            </a>
+            <a class="subheader" href="#">
+              AI/ML
+            </a>
+            <a class="subheader" href="#">
+              Mobile
+            </a>
+            <a class="subheader" href="#">
+              Other
+            </a>
+          </li>
+          <li>
+            <a class="header" href="#">
+              Education
+            </a>
+            <a class="subheader" href="#">
+              Courses
+            </a>
+          </li>
+          <li>
+            <a class="header" href="#">
+              Experience
+            </a>
+            <a class="subheader" href="#">
+              Professional
+            </a>
+            <a class="subheader" href="#">
+              Volunteer
+            </a>
+          </li>
+          <li>
+            <a class="header" href="#">
+              Blog
+            </a>
+            <a class="subheader" href="#">
+              Sept 2024
+            </a>
+          </li>
+          <li>
+            <a class="header" href="#">
+              Contact
+            </a>
+            <a class="subheader" href="#">
+              GitHub
+            </a>
+            <a class="subheader" href="#">
+              Gmail
+            </a>
+            <a class="subheader" href="#">
+              Linkedin
+            </a>
+
+            <div class="social_media">
+              <a href="https://github.com/channacy" target="_blank">
+                <img src="assets/github.png" />
+              </a>
+              <a href="mailto:channacyun@gmail.com" target="_blank">
+                <img src="assets/gmail.png" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/channacy-un/"
+                target="_blank"
+              >
+                <img src="assets/linkedin.png" />
+              </a>
+            </div>
+          </li>
+        </ul>
       </div>
+    </>
+  );
+};
 
-        </div>
-        
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
