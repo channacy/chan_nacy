91fcf4c2809c41e55393390caa3f0f6a49a1ace1
channacy <68759286+channacy@users.noreply.github.com>
2024-09-29 12:25:47 -0400

    add more configurations

--- channacy/app.config.ts
+++ channacy/app.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
+  start: {
+    server: {
+      preset: "cloudflare-pages",
+      // We will need to enable CF Pages node compatiblity
+      // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+      rollupConfig: {
+        external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+      },
+    },
+  },
   vite: {
     plugins: [UnoCSS()]
   }
